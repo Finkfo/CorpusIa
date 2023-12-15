1201201e8ecce9c84899c634327263de2237742f
Eliott <elacroix@gaming.tech>
2023-12-15 13:14:08 +0100

    maj

---
+++ frontend/corpusia/src/components/viewer/page_turn.js
@@ -0,0 +1,13 @@
+import "./style/page_turn.css"
+
+function Page_turn() {
+    return <div>
+
+        <div class='screen_bar'>
+            <img src='/img/top_bar.png' alt="top bar" />
+            <img src='/img/qround_bar.png' alt="around bar" />
+        </div>
+    </div>
+}
+
+export default Page_turn;
