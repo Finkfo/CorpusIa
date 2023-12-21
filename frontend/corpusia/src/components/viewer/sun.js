f8a9e9c32a8410e14ed4d64bf3cd4f49c4c8aec5
Eliott <elacroix@gaming.tech>
2023-12-21 14:19:51 +0100

    maj

--- frontend/corpusia/src/components/viewer/sun.js
+++ frontend/corpusia/src/components/viewer/sun.js
@@ -6,17 +6,17 @@ const SunComponent = () => {
             <div class="sun_settings">
                 <div class="line1" />
                 <h4>Sun X Orientation :</h4>
-                <span>0 </span><input type="range" min="0" max="360" value="240" class="slider" id="sunXRange" step="5" /> <span> 360</span>
-                <p>Your input : <span id="sunXOutput"></span></p>
+                <span>0 </span><input type="range" min="0" max="360" value="240" class="slider" id={window.sunXRange} step="5" /> <span> 360</span>
+                <p>Your input : <span id={window.sunXOutput}></span></p>
                 <div class="line1" />
                 <h4>Sun Y Orientation :</h4>
-                <span>0 </span><input type="range" min="0" max="360" value="0" class="slider" id="sunYRange" step="5" /> <span> 360</span>
-                <p>Your input : <span id="sunYOutput"></span></p>
+                <span>0 </span><input type="range" min="0" max="360" value="0" class="slider" id={window.sunYRange} step="5" /> <span> 360</span>
+                <p>Your input : <span id={window.sunYOutput}></span></p>
                 <div class="line1" />
                 <h4>Sun Z Orientation :</h4>
-                <span>0 </span><input type="range" min="0" max="360" value="0" class="slider" id="sunZRange" step="5" /> <span> 360</span>
-                <p>Your input : <span id="sunZOutput"></span></p>
-                <button onclick="getSun()">Get Sun</button>
+                <span>0 </span><input type="range" min="0" max="360" value="0" class="slider" id={window.sunZRange} step="5" /> <span> 360</span>
+                <p>Your input : <span id={window.sunZOutput}></span></p>
+                <button onclick={() => window.getSun()}>Get Sun</button>
             </div>
         </div >
     );
